c86308db33682bd52444e1b5e388e45258e58e34
george koutsodimakis <gkoutsodimakis@novibet.com>
2022-06-21 01:39:04 +0300

    koin

---
+++ app/src/main/java/coo/apps/weather/KoinModule.kt
@@ -0,0 +1,9 @@
+package coo.apps.weather
+
+import org.koin.dsl.module
+
+class KoinModule(application: WeatherApplication) {
+    val modules = module {
+
+    }
+}
--- app/src/main/java/coo/apps/weather/WeatherApplication.kt
+++ app/src/main/java/coo/apps/weather/WeatherApplication.kt
@@ -1,13 +1,16 @@
 package coo.apps.weather
 
 import android.app.Application
-import com.google.android.libraries.places.api.Places
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class WeatherApplication: Application() {
+class WeatherApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // Initialize the SDK with the Google Maps Platform API key
-        Places.initialize(this, BuildConfig.MAPS_API_KEY)
+        startKoin {
+            androidContext(this@WeatherApplication)
+            modules(KoinModule(this@WeatherApplication).modules)
+        }
     }
 }
